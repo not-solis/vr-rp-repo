537aed71d768a8627df81c11cfa629456c0c7bd1
mfboulos <mboulos95@gmail.com>
2024-11-16 22:54:26 -0800

    Basic discord OAuth2 without auth callback

--- src/repo/Repo.tsx
+++ src/repo/Repo.tsx
@@ -1,24 +1,47 @@
+import OAuth2Login from 'react-simple-oauth2-login';
+
 import { DarkModeToggle } from '../components/DarkModeToggle';
-import { RoleplayApplicationProcess, RoleplayEntryProcess, RoleplayProject, RoleplayStatus } from '../components/RoleplayProject';
+import {
+  RoleplayApplicationProcess,
+  RoleplayEntryProcess,
+  RoleplayProject,
+  RoleplayStatus,
+} from '../components/RoleplayProject';
 import './Repo.css';
 
 export const App = () => {
+  //https://discord.com/oauth2/authorize?client_id=1307585790292787200&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=identify
+  //https://discord.com/oauth2/authorize?client_id=1307585790292787200&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=identify?client_id=1307585790292787200&scope=&redirect_uri=http://localhost:3000/&response_type=token
+  //https://discord.com/oauth2/authorize?client_id=1307585790292787200&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=identify?client_id=1307585790292787200&scope=&redirect_uri=http://localhost:3000/
   return (
     <div style={{ height: '100vh' }}>
       <div className='nav'>
         <header>VR Roleplay Repo </header>
         {/* <DarkModeToggle /> */}
+        <OAuth2Login
+          authorizationUrl='https://discord.com/oauth2/authorize'
+          responseType='token'
+          clientId='1307585790292787200'
+          redirectUri='http://localhost:3000/authcallback'
+          onSuccess={(data) => {
+            console.log(data);
+          }}
+          onFailure={(err) => {
+            console.log(err);
+          }}
+          scope='identify'
+        />
       </div>
       <div className='project-container'>
         {[
           {
             name: 'The Best Roleplay',
-            imageUrl: 'https://cdn.discordapp.com/attachments/1092610233509105665/1253827287363747931/image.png?ex=6679e828&is=667896a8&hm=30460601e4925ad3236b8c16249a59ee66ef151bc8bd0c12563f22d29459a247&',
-            description: 'This roleplay is incredible. It takes place in the most fantasy place with the most weapony weapons and more dice than you can humanly accept in a given setting.',
+            imageUrl:
+              'https://i.kym-cdn.com/photos/images/original/001/240/860/528.png',
+            description:
+              'This roleplay is incredible. It takes place in the most fantasy place with the most weapony weapons and more dice than you can humanly accept in a given setting.',
             tags: ['cool', 'nice'],
-            runtime: [
-              new Date()
-            ],
+            runtime: [new Date()],
             status: RoleplayStatus.Active,
             entryProcess: RoleplayEntryProcess.Open,
             applicationProcess: RoleplayApplicationProcess.NoApplication,
@@ -26,13 +49,14 @@ export const App = () => {
             isMetaverse: false,
             isQuestCompatible: true,
             discordUrl: 'http://www.google.com',
-            otherLinks: []
-          }
-        ].map(p => <RoleplayProject {...p} />)}
+            otherLinks: [],
+          },
+        ].map((p) => (
+          <RoleplayProject {...p} />
+        ))}
       </div>
     </div>
-
   );
-}
+};
 
 export default App;
