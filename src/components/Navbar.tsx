2c9de4a82129e61b9ab2fe48abba8efe54bacb21
mfboulos <mboulos95@gmail.com>
2025-01-11 02:48:37 -0800

    Use href for header link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,51 @@
-import { Box, Link, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Box, Link, useTheme } from '@mui/material';
 import { LoginComponent } from './LoginComponent';
 
 import './Navbar.css';
 
 export const Navbar = () => {
+  const theme = useTheme();
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <Box className='nav'>
-      <header onClick={() => (window.location.href = '/')}>
-        VR Roleplay Repo
-      </header>
+      <a
+        href='/'
+        style={{ color: theme.palette.text.primary, textDecoration: 'none' }}
+      >
+        <header>VR Roleplay Myriad</header>
+      </a>
       <Box className='main-menu'>
-        {['Repo', 'Blog', 'Resources', 'About Us'].map((t) => (
-          <Link
-            href={t.toLowerCase().replace(' ', '-')}
-            component='a'
-            variant='h6'
-            color='textSecondary'
-            underline='none'
-          >
-            {t}
-          </Link>
-        ))}
+        {windowDimensions.width > 1100 ? (
+          ['Repo', 'Blog', 'Resources', 'About Us'].map((t) => (
+            <Link
+              href={t.toLowerCase().replace(' ', '-')}
+              component='a'
+              variant='h6'
+              color='textSecondary'
+              underline='none'
+            >
+              {t}
+            </Link>
+          ))
+        ) : (
+          <div>---</div> // Replace with burger menu
+        )}
       </Box>
       <LoginComponent />
     </Box>
